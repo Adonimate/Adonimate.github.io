550e6d17951ef762993b1e08720045eab4f277be
Adonimate <57105574+Adonimate@users.noreply.github.com>
2020-05-12 11:18:52 +0300

    Add files via upload

---
+++ Lab3/task3.js
@@ -0,0 +1,31 @@
+//task3
+	const object1 = {
+		name: 'Boris',
+		weight: 15
+	}
+
+	const object2 = {
+		name: 'Boris',
+		weight: 15
+	}
+
+	const object3 = {
+		name: 'Rex',
+		weight: 20,
+	}
+
+    // Реализуйте функцию isEquivalent
+	var isEquivalent = function (firstObject, secondObject) {
+    if (firstObject === secondObject) {
+        return true;
+    } 
+    for (var key in firstObject) {
+        if (firstObject[key] != secondObject[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isEquivalent(object1, object2));
+console.log(isEquivalent(object1, object3));
