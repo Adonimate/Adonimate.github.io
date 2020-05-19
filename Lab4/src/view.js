15cfb7f6b3cc9cf6f36bc9cc2a3b231f6fa04fbe
Adonimate <57105574+Adonimate@users.noreply.github.com>
2020-05-19 23:51:57 +0300

    Add files via upload

---
+++ Lab4/src/view.js
@@ -0,0 +1,4 @@
+export function getAboutMe() {
+    document.getElementById('main').innerHTML = '<section id = "myInfo"> <h2 id = "myName">Эрдлей Алан</h2> <span id = "text">Лабораторная работа 4 М3307</span> </section>';
+}
+
